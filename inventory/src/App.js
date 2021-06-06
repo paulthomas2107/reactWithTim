61df96ef878763e23f01b8982c6990e1a6f3b98f
Paul Thomas <paulthomas2107@gmail.com>
2021-06-06 13:11:21 +0100

    Update

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,17 +1,32 @@
 import "./App.css";
 import Info from "./info";
-import AddItem from "./addItem";
-
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Info title="Paul's Stuff" />
       <Info />
-      <AddItem text="Paul!" number={101} myName="Thomas" />
-      <AddItem text="Rory" myName="Thomas" />
+      <ButtonState />
     </div>
   );
 }
 
+function ButtonState() {
+  const [title, setTitle] = useState("");
+  const [count, setCount] = useState(0);
+  const updateTitleClicked = () => {
+    setTitle("Now have title");
+  };
+  const updateCounterClicked = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <p>Title: {title}</p>
+      <p>Counter: {count}</p>
+      <button onClick={updateTitleClicked}>Update Title</button>
+      <button onClick={updateCounterClicked}>Update Counter</button>
+    </div>
+  );
+}
 export default App;
