1afc2b77aa22cf34ba3a6ee631bc30d3317710b3
Paul Thomas <paulthomas2107@gmail.com>
2021-06-13 15:43:02 +0100

    More

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,41 +1,22 @@
 import "./App.css";
-import Info from "./info";
+import SearchBar from "./SearchBar";
 import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  };
+
   return (
     <div className="App">
-      <Info />
-      <ButtonState />
+      <SearchBar callback={updateData} />
+      <p>Name: {"name" in data ? data["name"] : "No Name to Display"}</p>
+      <p>Price: {"price" in data ? data["price"] : "No Price to Display"}</p>
+      <p>Type: {"type" in data ? data["type"] : "No Type to Display"}</p>
+      <p>Brand: {"brand" in data ? data["brand"] : "No Brand to Display"}</p>
     </div>
   );
 }
 
-function Data(props) {
-  return (
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Counter: {props.count}</p>
-    </div>
-  );
-}
-
-function ButtonState() {
-  const [title, setTitle] = useState("");
-  const [count, setCount] = useState(0);
-  const updateTitleClicked = () => {
-    setTitle("Now have title");
-  };
-  const updateCounterClicked = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <Data title={title} count={count} />
-      <button onClick={updateTitleClicked}>Update Title</button>
-      <button onClick={updateCounterClicked}>Update Counter</button>
-    </div>
-  );
-}
 export default App;
