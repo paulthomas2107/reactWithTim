f3d8556e9e68190d169ca45997a82c20071a3ef4
Paul Thomas <paulthomas2107@gmail.com>
2021-06-15 20:53:00 +0100

    More

--- inventory/src/ItemsDisplay.js
+++ inventory/src/ItemsDisplay.js
@@ -1,19 +1,17 @@
 function ItemsDisplay(props) {
-  return (
-    <div>
-      {props.items.map((item) => {
-        return (
-          <div>
-            <p>Id: {item.id}</p>
-            <p>Name: {item.name}</p>
-            <p>Price: {item.price}</p>
-            <p>Type: {item.type}</p>
-            <p>Brand: {item.brand}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  const showItem = (item) => {
+    return (
+      <div>
+        <p>Id: {item.id}</p>
+        <p>Name: {item.name}</p>
+        <p>Price: {item.price}</p>
+        <p>Type: {item.type}</p>
+        <p>Brand: {item.brand}</p>
+      </div>
+    );
+  };
+  return <div>{props.items.map(showItem)}</div>;
 }
 
 export default ItemsDisplay;
+    
